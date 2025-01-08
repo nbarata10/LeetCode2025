0077c1f39c39d6c327a32d7bf866c3a87091ea5f
nbarata10 <nunobaratasilva@gmail.com>
2025-01-08 19:16:29 +0000

    Implementing the solution for the January 8th LeetCode daily challenge.

---
+++ src/main/java/leetcode/january/JanuaryEigth.java
@@ -0,0 +1,32 @@
+package main.java.leetcode.january;
+
+public class JanuaryEigth {
+    public static void main(String[] args) {
+        final String[] words = {"a","aba","ababa","aa"};
+        final String[] words1 = {"pa","papa","ma","mama"};
+        final String[] words2 = {"abab","ab"};
+
+        final int answer = countPrefixSuffixPairs(words2);
+
+        System.out.println("Number of Prefix And Sufix Pairs: " + answer);
+    }
+
+    public static int countPrefixSuffixPairs(String[] words) {
+        int numberOfPrefixSuffixPairs = 0;
+
+        for(int i=0; i < words.length; i++){
+            for(int j=i+1; j < words.length; j++) {
+                if(isPrefixAndSuffix(words[i], words[j])) {
+                    System.out.println("i=" + i + " and j=" + j + " are a pair.");
+                    numberOfPrefixSuffixPairs++;
+                }
+            }
+        }
+
+        return numberOfPrefixSuffixPairs;
+    }
+
+    public static Boolean isPrefixAndSuffix(final String stringToEvaluate, final String completeString) {
+        return completeString.startsWith(stringToEvaluate) && completeString.endsWith(stringToEvaluate);
+    }
+}
